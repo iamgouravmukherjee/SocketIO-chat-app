ce3e4558912753abd526e380a38ef674ddc29d44
GouravMukherjee <apple@Apples-MacBook-Pro.local>
2018-12-12 13:46:20 +0530

    first commit

--- server/server.js
+++ server/server.js
@@ -2,11 +2,11 @@ const path = require('path');
 const express = require('express');
 
 const publicPath = path.join(__dirname, '../public');
-const port = process.env.PORT || 3000;
+const port = process.env.port || 3000;
 var app = express();
 
 app.use(express.static(publicPath));
 
 app.listen(port, () => {
-  console.log(`Server is up on ${port}`);
-});
+  console.log('app running on port', port);
+})
